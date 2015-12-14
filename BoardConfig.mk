c7e46ee1900d7f2ccadcd2b2fec651b855b81437
huangqiwu <huangqiwu@xiaomi.com>
2015-12-14 14:32:05 +0800

    Update for FM
    Signed-off-by: huangqiwu <huangqiwu@xiaomi.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,6 @@
 
 #Audio
-#AUDIO_FEATURE_ENABLED_FM := true
+AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true
 
 # FM
 BOARD_HAVE_QCOM_FM := true
@@ -10,6 +10,8 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/xiaomi/cancro/bluetooth
 
 include device/xiaomi/msm8974-common/BoardConfigCommon.mk
 TARGET_RELEASETOOLS_EXTENSIONS := device/xiaomi/cancro
+
+TARGET_OTA_ASSERT_DEVICE := cancro
 
 # inherit from the proprietary version
 -include vendor/xiaomi/cancro/BoardConfigVendor.mk
